b832f541a7e1f35302d42e3130c21afebccd9287
Harshit Gupta <lcs2019008@iiitl.ac.in>
2020-12-19 13:25:39 +0530

    Pushed

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
-// const keys = require('../config/keys.js');
 
 mongoose.connect(
-    keys.MongoURI,
+    process.env.MongoURI || require('../config/keys.js').MongoURI,
     { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true },
     function(err){
         if(err) console.log(err);
